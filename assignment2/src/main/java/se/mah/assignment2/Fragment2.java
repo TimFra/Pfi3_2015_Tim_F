a5c2f757920c1e419e25328dfbffdb8c039a4c31
TimFra <tim.franzen@hotmail.com>
2015-04-09 03:34:54 +0200

    Assignment 2

---
+++ assignment2/src/main/java/se/mah/assignment2/Fragment2.java
@@ -0,0 +1,32 @@
+package se.mah.assignment2;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragment2 extends Fragment {
+
+
+    public Fragment2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =inflater.inflate(R.layout.fragment_fragment2, container, false);
+        return v;
+    }
+
+
+}
